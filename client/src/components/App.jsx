7b8a7fb00edd1e85c5d3bc48ccc2d57367ba86a5
Luke Hatcher <lukehatcher98@gmail.com>
2020-11-11 11:32:28 -0800

    react rendering properly

---
+++ client/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>React rendered</div>
+        );
+    }
+}
+
+export default App;
