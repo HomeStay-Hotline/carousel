8a6349e1726517d73fd1129c75fcfcba8900dca4
Luke Hatcher <lukehatcher98@gmail.com>
2020-11-12 00:51:24 -0800

    generating fake data and seeding script

--- database/index.js
+++ database/index.js
@@ -1,5 +1,18 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/carousel');
+mongoose.connect('mongodb://localhost/carousel', {useNewUrlParser: true, useUnifiedTopology: true})
+  .catch(err => {
+    console.log(err);
+  });
+
+const db = mongoose.connection;
+
+db.on("connected", () => {
+  console.log("connected to MongoDB");
+})
+
+db.on('error', err => {
+  console.log(err);
+});
 
 const placesSchema = new mongoose.Schema({
     id: Number,
@@ -26,3 +39,24 @@ const placesSchema = new mongoose.Schema({
     
 const Places = mongoose.model('places', placesSchema);
 const Activities = mongoose.model('activities', activitiesSchema);
+
+const createPlaceDoc = (data) => {
+  // creates 1 places doc for my Places collection
+  let doc = new Places(data);
+  doc.save()
+    .catch((err) => {
+      console.log('error saving place document', err);
+    })
+};
+
+const createActivityDoc = (data) => {
+  // creates 1 activity doc for my Activities collection
+  let doc = new Activities(data);
+  doc.save()
+    .catch((err) => {
+      console.log('error saving activity document', err);
+    })
+};
+
+module.exports.createPlaceDoc = createPlaceDoc;
+module.exports.createActivityDoc = createActivityDoc;
