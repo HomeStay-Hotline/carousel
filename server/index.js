65b614091e04b63850fac6df35e9b50775363e37
Luke Hatcher <lukehatcher98@gmail.com>
2020-11-12 21:37:08 -0800

    create GET routes for images/places and images/activities

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const db = require('../database/index.js');
 
 
 const PORT = 3000 || process.env.PORT;
@@ -8,7 +9,30 @@ const app = express();
 const PUB_DIR = path.resolve(__dirname, '..', 'public');
 
 app.use(express.static(PUB_DIR))
+app.use(morgan('dev'));
 app.use(express.json());
+
+app.get('/api/home/:id/images/places', (req, res) => {
+    let id = req.params.id;
+    db.fetchPlaceDatabyID(id)
+        .then((queryResult) => {
+            res.send(queryResult);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+app.get('/api/home/:id/images/activities', (req, res) => {
+    let id = req.params.id;
+    db.fetchActivityDatabyID(id)
+        .then((queryResult) => {
+            res.send(queryResult);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
 
 app.listen(PORT, (err) => {
     if (err) {
