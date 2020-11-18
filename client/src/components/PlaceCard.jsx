66cf1390cb850a36b989a4e34f6ba8bcd9a0e8af
Luke Hatcher <lukehatcher98@gmail.com>
2020-11-17 20:23:47 -0800

    implement like button functionality

--- client/src/components/PlaceCard.jsx
+++ client/src/components/PlaceCard.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles/PlaceCard.css';
 
 export default function PlaceCard({ place }) {
-  const emergancyURl = 'http://lorempixel.com/320/240/';
+  const [heartColor, setHeartColor] = useState(false);
+  const handleHeartClick = () => {
+    setHeartColor(!heartColor);
+    console.log(heartColor);
+  };
   return (
     <div className="placecard-container">
-      <div className="image-container">
+      <div className="place-image-container">
         <img className="place-img" src={place.url} alt="airbnb listing" />
-        <span className="heart-icon" />
+        <div onClick={handleHeartClick} className="p-heart-container">
+          {heartColor
+            ? <div className="place-heart-icon" style={{ background: 'red' }} />
+            : <div className="place-heart-icon" style={{ background: 'black' }} />}
+        </div>
       </div>
-      <div className="star-rating-container">
-        <span className="star-icon" />
+      <div className="place-rating-container">
+        <span className="place-star-icon" />
         <span className="place-rating">{place.rating}</span>
       </div>
       <p className="place-listing-type-beds">
