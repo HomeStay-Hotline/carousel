d4eb1d961ffdd2cb6f2e8546cc4b5b9bfc8fcb1c
Luke Hatcher <lukehatcher98@gmail.com>
2020-11-19 16:13:16 -0800

    complete coverage

--- __tests__/unit/PlaceHeader.test.js
+++ __tests__/unit/PlaceHeader.test.js
@@ -1,28 +1,76 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 import PlaceHeader from '../../client/src/components/PlaceHeader';
+import App from '../../client/src/components/App';
 
 describe('PlaceHeader', () => {
-  const handleArrowClick = jest.fn();
-  let wrapper;
-  beforeEach(() => {
-    const props = {
-      handleArrowClick,
-    };
-    wrapper = shallow(<PlaceHeader {...props}/>);
-  });
+  Element.prototype.scrollBy = jest.fn(); // jsdom does not have scrollBy built in
+
   it('should exist', () => {
-    expect(wrapper.find('.place-header-comp').exists()).toBe(true);
+    const wrapper = shallow(<PlaceHeader passedRef={{}} />);
+    expect(wrapper.exists()).toBe(true);
   });
-  it('should contain the right text', () => {
-    expect(wrapper.text().includes('More places to stay')).toBe(true);
+  describe('carousel movement', () => {
+    it('should call carousel movement function anytime the arrow buttons are clicked', () => {
+      const wrapper = mount(<App />);
+      const rightButton = wrapper.find('.left-button.place');
+      const leftButton = wrapper.find('.right-button.place');
+      wrapper.setProps({}); // needed for the ref
+      rightButton.simulate('click');
+      leftButton.simulate('click');
+      expect(Element.prototype.scrollBy).toHaveBeenCalledTimes(2);
+      jest.clearAllMocks();
+    });
   });
-  it('should contain two buttons', () => {
-    expect(wrapper.find('button')).toHaveLength(2);
-  });
-  it('should call click handler method when buttons are clicked', () => {
-    const button = wrapper.find('button').at(0);
-    button.simulate('click');
-    expect(handleArrowClick).toBeCalled();
+  describe('arrow buttons', () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = mount(<App />);
+    });
+    describe('right arrow', () => {
+      it('should update count by one when right button is clicked once', () => {
+        const button = wrapper.find('.right-button.place');
+        wrapper.setProps({}); // needed to force rerender
+        button.simulate('click');
+        const pageCount = wrapper.find('.page-count').at(0).text().slice(0, 1);
+        expect(pageCount).toBe('2');
+        button.simulate('click');
+        const pageCount2 = wrapper.find('.page-count').at(0).text().slice(0, 1);
+        expect(pageCount2).toBe('3');
+        jest.clearAllMocks();
+      });
+      it('should reset count after two right clicks', () => {
+        const button = wrapper.find('.right-button.place');
+        wrapper.setProps({}); // needed to force make sure ref exists
+        button.simulate('click');
+        button.simulate('click');
+        button.simulate('click');
+        const pageCount = wrapper.find('.page-count').at(0).text().slice(0, 1);
+        expect(pageCount).toBe('1');
+        jest.clearAllMocks();
+      });
+    });
+    describe('left arrow', () => {
+      it('should reset count to page on inital click', () => {
+        const button = wrapper.find('.left-button.place');
+        wrapper.setProps({}); // set ref
+        button.simulate('click');
+        const pageCount = wrapper.find('.page-count').at(0).text().slice(0, 1);
+        expect(pageCount).toBe('3');
+        jest.clearAllMocks();
+      });
+      it('should subtract page count by one after initial click', () => {
+        const button = wrapper.find('.left-button.place');
+        wrapper.setProps({}); // set ref
+        button.simulate('click');
+        button.simulate('click');
+        const pageCount = wrapper.find('.page-count').at(0).text().slice(0, 1);
+        expect(pageCount).toBe('2');
+        button.simulate('click');
+        const pageCount2 = wrapper.find('.page-count').at(0).text().slice(0, 1);
+        expect(pageCount2).toBe('1');
+        jest.clearAllMocks();
+      });
+    });
   });
 });
